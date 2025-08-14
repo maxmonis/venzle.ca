843f7c97ebe200b7b1c8ecbac3c1c7313927358e
Max <mmonis77@gmail.com>
2025-08-14 18:34:45 -0400

    Add other games

--- src/games.ts
+++ src/games.ts
@@ -19,5 +19,53 @@ export let gameList: Array<Game> = [
     },
     hint: "Geography, Government, Language",
     title: "Countries"
+  },
+  {
+    groups: {
+      "Led a Nation During Wartime": [
+        "Abraham Lincoln",
+        "Winston Churchill",
+        "Czar Nicholas II",
+        "Adolf Hitler"
+      ],
+      "Killed with a Firearm": [
+        "Abraham Lincoln",
+        "Mahatma Gandhi",
+        "Czar Nicholas II",
+        "Shinzo Abe"
+      ],
+      "Appears on Modern Currency": [
+        "Abraham Lincoln",
+        "Mahatma Gandhi",
+        "Winston Churchill",
+        "Thomas Jefferson"
+      ]
+    },
+    hint: "Conflict, Death, Money",
+    title: "World Leaders"
+  },
+  {
+    groups: {
+      "Oscar Winner": [
+        "Daniel Day-Lewis",
+        "Rachel Weisz",
+        "Tom Hanks",
+        "Meryl Streep"
+      ],
+      British: [
+        "Daniel Day-Lewis",
+        "Rachel Weisz",
+        "Alfred Molina",
+        "Idris Elba"
+      ],
+      "Born in the 1950s": [
+        "Daniel Day-Lewis",
+        "Tom Hanks",
+        "Alfred Molina",
+        "Bruce Willis"
+      ]
+    },
+    hint: "Award, Country, Decade",
+    title: "Actors"
   }
 ]
